001c89d80cf5aca8c8dbfdaeb65bd8de9556608d
Parveen Saroha <parveensaroha2002@gmail.com>
2025-07-22 16:04:03 +0530

    langchain output parser

---
+++ LangChainOutputParser/stroutputparsers_gemini.py
@@ -0,0 +1,34 @@
+from dotenv import load_dotenv
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+
+# Load .env
+load_dotenv()
+
+# ✅ Set up Gemini model
+model = ChatGoogleGenerativeAI(
+    model="models/gemini-1.5-flash-latest",
+    google_api_key=os.getenv("GEMINI_API_KEY")
+)
+
+# 1st prompt -> detailed report
+template1 = PromptTemplate(
+    template='Write a detailed report on {topic}',
+    input_variables=['topic']
+)
+
+# 2nd prompt -> summary
+template2 = PromptTemplate(
+    template='Write a 5 line summary on the following text:\n{text}',
+    input_variables=['text']
+)
+
+# Format and invoke the prompts
+prompt1 = template1.invoke({'topic': 'black hole'})
+result = model.invoke(prompt1)
+
+prompt2 = template2.invoke({'text': result.content})
+result1 = model.invoke(prompt2)
+
+print(result1.content)
